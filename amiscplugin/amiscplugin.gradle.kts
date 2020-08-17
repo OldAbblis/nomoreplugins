2d216e5917d76488c35e8ef98f8af198a2e437b9
Thoma <thomasbiggin94+github@gmail.com>
2020-08-17 10:33:54 +0100

    Reband to NoMore.

--- amiscplugin/amiscplugin.gradle.kts
+++ amiscplugin/amiscplugin.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "0.0.1"
+version = "0.1.0"
 
 project.extra["PluginName"] = "A Misc Plugin"
 project.extra["PluginDescription"] = "Numerous miscellaneous plugins."
